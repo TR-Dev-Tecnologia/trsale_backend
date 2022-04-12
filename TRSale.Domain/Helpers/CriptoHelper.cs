3138a66249c891c6ad3d1becdeec7132e1ef419a
Edgar <edgar@onsist.com.br>
2022-04-12 17:39:53 -0300

    CRIPT Password sha256

--- TRSale.Domain/Helpers/CriptoHelper.cs
+++ TRSale.Domain/Helpers/CriptoHelper.cs
@@ -21,27 +21,28 @@ namespace TRSale.Domain.Helpers
         {  
             if (password == null)
             {
-                throw new ArgumentNullException("Error please check psw");
+                throw new ArgumentNullException("password is null");
             }                     
       
             
-            byte[] salt = new Rfc2898DeriveBytes(password, 16).Salt;
-            byte[] buffer2;
-   
-            using (Rfc2898DeriveBytes bytes = new Rfc2898DeriveBytes(password, 0x10, 0x3e8))
-            {
-                salt = bytes.Salt;
-                buffer2 = bytes.GetBytes(0x20);
+            // Create a SHA256   
+            using (SHA256 sha256Hash = SHA256.Create())  
+            {  
+                // ComputeHash - returns byte array  
+                byte[] bytes = sha256Hash.ComputeHash(Encoding.UTF8.GetBytes(password));  
+  
+                // Convert byte array to a string   
+                StringBuilder builder = new StringBuilder();  
+                for (int i = 0; i < bytes.Length; i++)  
+                {  
+                    builder.Append(bytes[i].ToString("x2"));  
+                }  
+                return Convert.ToBase64String(Encoding.UTF8.GetBytes(builder.ToString()));  
             }
-            byte[] dst = new byte[0x31];
-            Buffer.BlockCopy(salt, 0, dst, 1, 0x10);
-            Buffer.BlockCopy(buffer2, 0, dst, 0x11, 0x20);
-            return Convert.ToBase64String(dst);
         }
 
         public static bool VerifyHashedPassword(string? hashedPassword, string? password)
         {
-            byte[] buffer4;
             if (hashedPassword == null)
             {
                 return false;
@@ -50,21 +51,8 @@ namespace TRSale.Domain.Helpers
             {
                 throw new ArgumentNullException("Psw error");
             }
-            byte[] src = Convert.FromBase64String(hashedPassword);
-            if ((src.Length != 0x31) || (src[0] != 0))
-            {
-                return false;
-            }
-            byte[] dst = new byte[0x10];
-            Buffer.BlockCopy(src, 1, dst, 0, 0x10);
-            byte[] buffer3 = new byte[0x20];
-            Buffer.BlockCopy(src, 0x11, buffer3, 0, 0x20);
 
-            using (Rfc2898DeriveBytes bytes = new Rfc2898DeriveBytes(password, dst, 0x3e8))
-            {
-                buffer4 = bytes.GetBytes(0x20);
-            }
-            return ByteArraysEqual(buffer3, buffer4);
+            return CriptoHelper.HashPassword(password) == hashedPassword;
         }
 
         public static string EncryptString(string plainText, string key, string keyIv)
