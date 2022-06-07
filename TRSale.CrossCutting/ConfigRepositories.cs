0ee3946270a5f40cde5fb9b94d35b6db01cd6f7b
Edgar <edgar@onsist.com.br>
2022-06-07 14:43:01 -0300

    Add cross cutting project

---
+++ TRSale.CrossCutting/ConfigRepositories.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using TRSale.DataBase;
+using TRSale.DataBase.Repositories;
+using TRSale.Domain.Interfaces.Infra;
+using TRSale.Domain.Interfaces.Repositories;
+
+namespace TRSale.CrossCutting
+{
+    public class ConfigRepositories
+    {
+        public static void Config(IServiceCollection services)
+        {
+            var connectionString = Environment.GetEnvironmentVariable("Connection_db");
+
+            services.AddDbContext<TRSaleContext>(options => options.UseMySql(connectionString, new MySqlServerVersion(new Version(8, 0, 26))));
+
+            services.AddScoped<TRSaleContext, TRSaleContext>();
+
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+            services.AddScoped<IUserRepository, UserRepository>();
+            
+            
+        }
+        
+    }
+}
